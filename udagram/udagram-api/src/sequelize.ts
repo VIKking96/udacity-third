33f8d454ecf2ba7fbf9524078924720e9ea650c9
VIKking96 <62469608+VIKking96@users.noreply.github.com>
2022-10-21 23:47:41 +0200

    udacity

---
+++ udagram/udagram-api/src/sequelize.ts
@@ -0,0 +1,8 @@
+import { Sequelize } from "sequelize-typescript";
+import { config } from "./config/config";
+
+export const sequelize = new Sequelize(
+  `postgres://${config.username}:${config.password}@${config.host}:${config.DB_PORT}/${config.database}`
+);
+
+  
